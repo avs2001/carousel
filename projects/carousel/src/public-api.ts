ce20343101718215966ae49f3596c35ae19317eb
Ciprian Iuga <ciprian@>
2022-08-19 08:05:15 +0300

    add carousel

--- projects/carousel/src/public-api.ts
+++ projects/carousel/src/public-api.ts
@@ -7,5 +7,5 @@ export * from './lib/carousel.module';
 export * from './lib/carousel-scroll.directive';
 export * from './lib/carousel-auto-scroll.directive';
 export * from './lib/carousel/carousel.directive';
-export * from './lib/carousel/carousel.directive';
+export * from './lib/carousel/carousel.component';
 export * from './lib/carousel/carousel-item.directive';
