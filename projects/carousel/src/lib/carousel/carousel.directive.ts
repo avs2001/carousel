5e950c2816f785932a86d7725964f88490b78fb5
Ciprian Iuga <ciprian@kebormed.com>
2025-06-12 14:10:52 +0300

    chore: upgrade to angular 20 with signals

--- projects/carousel/src/lib/carousel/carousel.directive.ts
+++ projects/carousel/src/lib/carousel/carousel.directive.ts
@@ -1,15 +1,13 @@
-import {Directive, ElementRef, Inject, Input} from '@angular/core';
+import {Directive, ElementRef, Inject, Input, signal, computed} from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import {
-  BehaviorSubject,
-  combineLatest,
   EMPTY,
   fromEvent,
   interval,
   mapTo,
   merge,
   Observable,
-  switchMap,
-  tap
+  switchMap
 } from "rxjs";
 
 @Directive({
@@ -18,29 +16,32 @@ import {
 })
 export class CarouselDirective extends Observable<unknown> {
 
-  private readonly duration$ = new BehaviorSubject(0);
+  private readonly duration = signal(0);
+  private readonly running = signal(false);
 
-  private readonly running$ = merge(
-    fromEvent(this.elementRef.nativeElement, 'mouseenter').pipe(mapTo(false)),
-    fromEvent(this.elementRef.nativeElement, 'touchstart').pipe(mapTo(false)),
-    fromEvent(this.elementRef.nativeElement, 'touchend').pipe(mapTo(true)),
-    fromEvent(this.elementRef.nativeElement, 'mouseleave').pipe(mapTo(true)),
-  );
-
-  private readonly output$ = combineLatest([this.duration$, this.running$]).pipe(
-    switchMap(([duration, running]) =>
+  private readonly output$ = toObservable(
+    computed(() => ({duration: this.duration(), running: this.running()}))
+  ).pipe(
+    switchMap(({duration, running}) =>
       duration && running ? interval(duration) : EMPTY,
     ),
   );
 
   @Input()
   set duration(duration: number) {
-    this.duration$.next(duration);
+    this.duration.set(duration);
   }
 
   constructor(
     @Inject(ElementRef) private readonly elementRef: ElementRef<HTMLElement>,
   ) {
+    merge(
+      fromEvent(this.elementRef.nativeElement, 'mouseenter').pipe(mapTo(false)),
+      fromEvent(this.elementRef.nativeElement, 'touchstart').pipe(mapTo(false)),
+      fromEvent(this.elementRef.nativeElement, 'touchend').pipe(mapTo(true)),
+      fromEvent(this.elementRef.nativeElement, 'mouseleave').pipe(mapTo(true)),
+    ).subscribe(v => this.running.set(v));
+
     super(subscriber => this.output$.subscribe(subscriber));
   }
 
