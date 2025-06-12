001e54dbce44399d60e849a1e1bc1b741ca4503b
Ciprian Iuga <ciprian@kebormed.com>
2025-06-12 13:59:55 +0300

    feat: add basic survey builder

--- projects/survey-builder/src/lib/survey-builder.module.ts
+++ projects/survey-builder/src/lib/survey-builder.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SurveyBuilderComponent } from './survey-builder.component';
 
 @NgModule({
   declarations: [SurveyBuilderComponent],
-  imports: [],
-  exports: [SurveyBuilderComponent]
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [SurveyBuilderComponent],
 })
 export class SurveyBuilderModule {}
--- projects/survey-builder/src/lib/survey-builder.service.ts
+++ projects/survey-builder/src/lib/survey-builder.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { SurveyQuestion } from './survey-builder.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SurveyBuilderService {
-  constructor() { }
+  private STORAGE_KEY = 'survey-builder-draft';
+
+  saveDraft(questions: SurveyQuestion[]): void {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(questions));
+  }
+
+  loadDraft(): SurveyQuestion[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? (JSON.parse(data) as SurveyQuestion[]) : [];
+  }
 }
