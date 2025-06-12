f33e1e8d0197467c08f1fd5682b15ca920c26ae1
Ciprian Iuga <ciprian@kebormed.com>
2025-06-12 13:54:08 +0300

    Add survey-builder project

---
+++ projects/survey-builder/src/lib/survey-builder.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SurveyBuilderComponent } from './survey-builder.component';
+
+@NgModule({
+  declarations: [SurveyBuilderComponent],
+  imports: [],
+  exports: [SurveyBuilderComponent]
+})
+export class SurveyBuilderModule {}
---
+++ projects/survey-builder/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of survey-builder
+ */
+
+export * from './lib/survey-builder.service';
+export * from './lib/survey-builder.component';
+export * from './lib/survey-builder.module';
