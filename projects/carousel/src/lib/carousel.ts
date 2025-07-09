ddbe52f9a9f75055c08d5f0632f80e02e64a788c
Iuga Ciprian <ciprian@>
2025-07-09 21:39:29 +0300

    .

---
+++ projects/carousel/src/lib/carousel.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-carousel',
+  imports: [],
+  template: `
+    <p>
+      carousel works!
+    </p>
+  `,
+  styles: ``
+})
+export class Carousel {
+
+}
